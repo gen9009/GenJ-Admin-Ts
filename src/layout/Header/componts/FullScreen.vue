044072b5b756b28af21d29646e8702fdd7cba3f0
Jiang <15971199009@163.com>
2022-08-02 10:58:35 +0800

    feat:新增vueUse全屏展示

--- src/layout/Header/componts/FullScreen.vue
+++ src/layout/Header/componts/FullScreen.vue
@@ -1,7 +1,10 @@
 <template>
   <el-tooltip content="全屏展示">
-    <span class="iconfont icon-quanping"></span>
+    <span class="iconfont" :class="isFullscreen ? 'icon-tuichuquanping' : 'icon-quanping'" @click="toggle"></span>
   </el-tooltip>
 </template>
-<script setup></script>
+<script setup lang="ts">
+import { useFullscreen } from '@vueuse/core';
+const { toggle, isFullscreen } = useFullscreen();
+</script>
 <style lang="scss" scoped></style>
